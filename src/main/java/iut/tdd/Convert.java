113264f8b2b6bdc66c15e5929a6fd580da4155e4
Cyril Gerard <gerardcyril19@gmail.com>
2015-04-17 15:59:33 +0200

    salut

--- src/main/java/iut/tdd/Convert.java
+++ src/main/java/iut/tdd/Convert.java
@@ -1,59 +1,103 @@
 package iut.tdd;
 
 public class Convert {
-
-	public Convert() {
-
-	}
-
 	public static String num2text(String input) {
-		int taille = input.length();
-		switch (input) {
-		case "0":
+		switch (Integer.parseInt(input)) {
+		case 0:
 			return "zéro";
-		case "1":
+		case 1:
 			return "un";
-		case "2":
+
+		case 2:
 			return "deux";
-		case "3":
+
+		case 3:
 			return "trois";
-		case "4":
+
+		case 4:
 			return "quatre";
-		case "5":
+
+		case 5:
 			return "cinq";
-		case "6":
+
+		case 6:
 			return "six";
-		case "7":
+
+		case 7:
 			return "sept";
-		case "8":
+
+		case 8:
 			return "huit";
-		case "9":
+
+		case 9:
 			return "neuf";
-		case "10":
+		case 10:
 			return "dix";
-		case "11":
+		case 11:
 			return "onze";
-		case "12":
+		case 12:
 			return "douze";
-		case "13":
+		case 13:
 			return "treize";
-		case "14":
+		case 14:
 			return "quatorze";
-		case "15":
+		case 15:
 			return "quinze";
-		case "16":
+		case 16:
 			return "seize";
-		case "17":
-			return "dix-sept";
-		case "18":
-			return "dix-huit";
-		case "19":
-			return "dix-neuf";
-		case "20":
+		case 20:
 			return "vingt";
+		case 30:
+			return "trente";
+		case 40:
+			return "quarante";
+		case 50:
+			return "cinquante";
+		case 60:
+			return "soixante";
+		case 80:
+			return "quatre-vingt";
+
+		default:
+			return trad(input);
 
 		}
-		return null;
+	}
+
+	private static String trad(String input) {
+		String res = "";
+		switch (input.length()) {
+		case 3:
+			return num2text(input.charAt(0) + "") + " cent "
+					+ num2text(input.substring(1));
+
+		default:
+
+			switch (input.charAt(0)) {
+
+			case '7':
+				res += num2text("60") + "-";
+				if (input.charAt(1) == '1') {
+					res += "et-";
+				}
+				res += num2text("1" + input.charAt(1));
+				break;
+			case '9':
+				res += num2text("80") + "-";
+				res += num2text("1" + input.charAt(1));
+				break;
+			default:
+				switch (input.charAt(1)) {
+				case '1':
+					return num2text(input.charAt(0) + "0") + "-et-un";
+
+				default:
+					return num2text(input.charAt(0) + "0") + "-"
+							+ num2text("" + input.charAt(1));
+				}
+			}
+		}
+		return res;
 	}
 
 	public static String text2num(String input) {
--- src/test/java/iut/tdd/TestConvert.java
+++ src/test/java/iut/tdd/TestConvert.java
@@ -5,25 +5,230 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 public class TestConvert {
+
 	@Test
-	public void test_num2text_zero() {
+	public void test_num2text_0() {
 		Assert.assertEquals("zéro", Convert.num2text("0"));
 	}
 
 	@Test
-	public void test_num2text_onze() {
+	public void test_num2text_1() {
+		Assert.assertEquals("un", Convert.num2text("1"));
+	}
+
+	@Test
+	public void test_num2text_2() {
+		Assert.assertEquals("deux", Convert.num2text("2"));
+	}
+
+	@Test
+	public void test_num2text_3() {
+		Assert.assertEquals("trois", Convert.num2text("3"));
+	}
+
+	@Test
+	public void test_num2text_4() {
+		Assert.assertEquals("quatre", Convert.num2text("4"));
+	}
+
+	@Test
+	public void test_num2text_5() {
+		Assert.assertEquals("cinq", Convert.num2text("5"));
+	}
+
+	@Test
+	public void test_num2text_6() {
+		Assert.assertEquals("six", Convert.num2text("6"));
+	}
+
+	@Test
+	public void test_num2text_7() {
+		Assert.assertEquals("sept", Convert.num2text("7"));
+	}
+
+	@Test
+	public void test_num2text_8() {
+		Assert.assertEquals("huit", Convert.num2text("8"));
+	}
+
+	@Test
+	public void test_num2text_9() {
+		Assert.assertEquals("neuf", Convert.num2text("9"));
+	}
+
+	@Test
+	public void test_num2text_10() {
+		Assert.assertEquals("dix", Convert.num2text("10"));
+	}
+
+	@Test
+	public void test_num2text_11() {
 		Assert.assertEquals("onze", Convert.num2text("11"));
 	}
 
 	@Test
-	public void test_num2text_dixhuit() {
-		Assert.assertEquals("dix-huit", Convert.num2text("18"));	
-	}
-	
-	@Test
-	public void test_num2text_vingt() {
-		Assert.assertEquals("vingt", Convert.num2text("20"));	
+	public void test_num2text_12() {
+		Assert.assertEquals("douze", Convert.num2text("12"));
 	}
 
-	
+	@Test
+	public void test_num2text_13() {
+		Assert.assertEquals("treize", Convert.num2text("13"));
+	}
+
+	@Test
+	public void test_num2text_14() {
+		Assert.assertEquals("quatorze", Convert.num2text("14"));
+	}
+
+	@Test
+	public void test_num2text_15() {
+		Assert.assertEquals("quinze", Convert.num2text("15"));
+	}
+
+	@Test
+	public void test_num2text_16() {
+		Assert.assertEquals("seize", Convert.num2text("16"));
+	}
+
+	@Test
+	public void test_num2text_17() {
+		Assert.assertEquals("dix-sept", Convert.num2text("17"));
+	}
+
+	@Test
+	public void test_num2text_18() {
+		Assert.assertEquals("dix-huit", Convert.num2text("18"));
+	}
+
+	@Test
+	public void test_num2text_19() {
+		Assert.assertEquals("dix-neuf", Convert.num2text("19"));
+	}
+
+	@Test
+	public void test_num2text_20() {
+		Assert.assertEquals("vingt", Convert.num2text("20"));
+	}
+
+	@Test
+	public void test_num2text_21() {
+		Assert.assertEquals("vingt-et-un", Convert.num2text("21"));
+	}
+
+	@Test
+	public void test_num2text_22() {
+		Assert.assertEquals("vingt-deux", Convert.num2text("22"));
+	}
+
+	@Test
+	public void test_num2text_23() {
+		Assert.assertEquals("vingt-trois", Convert.num2text("23"));
+	}
+
+	@Test
+	public void test_num2text_24() {
+		Assert.assertEquals("vingt-quatre", Convert.num2text("24"));
+	}
+
+	@Test
+	public void test_num2text_25() {
+		Assert.assertEquals("vingt-cinq", Convert.num2text("25"));
+	}
+
+	@Test
+	public void test_num2text_26() {
+		Assert.assertEquals("vingt-six", Convert.num2text("26"));
+	}
+
+	@Test
+	public void test_num2text_27() {
+		Assert.assertEquals("vingt-sept", Convert.num2text("27"));
+	}
+
+	@Test
+	public void test_num2text_28() {
+		Assert.assertEquals("vingt-huit", Convert.num2text("28"));
+	}
+
+	@Test
+	public void test_num2text_29() {
+		Assert.assertEquals("vingt-neuf", Convert.num2text("29"));
+	}
+
+	@Test
+	public void test_num2text_30() {
+		Assert.assertEquals("trente", Convert.num2text("30"));
+	}
+
+	@Test
+	public void test_num2text_31() {
+		Assert.assertEquals("trente-et-un", Convert.num2text("31"));
+	}
+
+	@Test
+	public void test_num2text_32() {
+		Assert.assertEquals("trente-deux", Convert.num2text("32"));
+	}
+
+	@Test
+	public void test_num2text_33() {
+		Assert.assertEquals("trente-trois", Convert.num2text("33"));
+	}
+
+	@Test
+	public void test_num2text_34() {
+		Assert.assertEquals("trente-quatre", Convert.num2text("34"));
+	}
+
+	@Test
+	public void test_num2text_35() {
+		Assert.assertEquals("trente-cinq", Convert.num2text("35"));
+	}
+
+	@Test
+	public void test_num2text_36() {
+		Assert.assertEquals("trente-six", Convert.num2text("36"));
+	}
+
+	@Test
+	public void test_num2text_37() {
+		Assert.assertEquals("trente-sept", Convert.num2text("37"));
+	}
+
+	@Test
+	public void test_num2text_38() {
+		Assert.assertEquals("trente-huit", Convert.num2text("38"));
+	}
+
+	@Test
+	public void test_num2text_39() {
+		Assert.assertEquals("trente-neuf", Convert.num2text("39"));
+	}
+
+	@Test
+	public void test_num2text_40() {
+		Assert.assertEquals("quarante", Convert.num2text("40"));
+	}
+
+	@Test
+	public void test_num2text_71() {
+		Assert.assertEquals("soixante-et-onze", Convert.num2text("71"));
+	}
+
+	@Test
+	public void test_num2text_98() {
+		Assert.assertEquals("quatre-vingt-dix-huit", Convert.num2text("98"));
+	}
+
+	@Test
+	public void test_num2text_404() {
+		Assert.assertEquals("quatre cent quatre", Convert.num2text("404"));
+	}
+
+	@Test
+	public void test_num2text_231() {
+		Assert.assertEquals("deux cent trente-et-un", Convert.num2text("231"));
+	}
+
 }
