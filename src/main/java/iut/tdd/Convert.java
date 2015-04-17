a2c710af7279bcc697609fa3373f2377ebb7c39b
Cyril Gerard <gerardcyril19@gmail.com>
2015-04-17 15:02:41 +0200

    salut

--- src/main/java/iut/tdd/Convert.java
+++ src/main/java/iut/tdd/Convert.java
@@ -1,9 +1,61 @@
 package iut.tdd;
 
 public class Convert {
+
+	public Convert() {
+
+	}
+
 	public static String num2text(String input) {
+		int taille = input.length();
+		switch (input) {
+		case "0":
+			return "zéro";
+		case "1":
+			return "un";
+		case "2":
+			return "deux";
+		case "3":
+			return "trois";
+		case "4":
+			return "quatre";
+		case "5":
+			return "cinq";
+		case "6":
+			return "six";
+		case "7":
+			return "sept";
+		case "8":
+			return "huit";
+		case "9":
+			return "neuf";
+		case "10":
+			return "dix";
+		case "11":
+			return "onze";
+		case "12":
+			return "douze";
+		case "13":
+			return "treize";
+		case "14":
+			return "quatorze";
+		case "15":
+			return "quinze";
+		case "16":
+			return "seize";
+		case "17":
+			return "dix-sept";
+		case "18":
+			return "dix-huit";
+		case "19":
+			return "dix-neuf";
+		case "20":
+			return "vingt";
+
+		}
 		return null;
 	}
+
 	public static String text2num(String input) {
 		return null;
 	}
--- src/test/java/iut/tdd/TestConvert.java
+++ src/test/java/iut/tdd/TestConvert.java
@@ -6,15 +6,23 @@ import org.junit.Test;
 
 public class TestConvert {
 	@Test
-	public void test_num2text_zero () {
-		// Given
-		String input = "0";
-		String expected = "zéro";
-		Convert myConvert = new Convert();
-		// When
-		String actual = myConvert.num2text(input);
-		// Then
-		Assert.assertEquals(expected, actual);
+	public void test_num2text_zero() {
+		Assert.assertEquals("zéro", Convert.num2text("0"));
+	}
+
+	@Test
+	public void test_num2text_onze() {
+		Assert.assertEquals("onze", Convert.num2text("11"));
+	}
+
+	@Test
+	public void test_num2text_dixhuit() {
+		Assert.assertEquals("dix-huit", Convert.num2text("18"));	
 	}
 	
+	@Test
+	public void test_num2text_vingt() {
+		Assert.assertEquals("vingt", Convert.num2text("20"));	
+	}
+
 }
